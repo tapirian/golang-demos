99a4d01a00a153785280691d178821c0c7922de2
tuojinxin <15998186418@163.com>
2024-04-08 17:03:26 +0800

    add echo server

---
+++ helloworld/14-echo-server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handConn(conn)
+	}
+}
+
+func echo(conn net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(conn, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(conn, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(conn, "\t", strings.ToLower(shout))
+}
+
+func handConn(conn net.Conn) {
+	defer conn.Close()
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		go echo(conn, input.Text()+"\n", 1*time.Second)
+	}
+
+}
