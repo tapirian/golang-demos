04a73d1536d9b5096437c84ad3577c1827e6824e
tuojinxin <15998186418@163.com>
2025-10-16 11:43:07 +0800

    update

--- pprof/main.go
+++ pprof/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"runtime"
+	"sync"
 	"time"
 )
 
@@ -20,7 +21,9 @@ func main() {
 	// go MockMemoryUse()
 	// go MockCPUUse()
 	// go MockGoroutineLeak()
-	go MockGCfreq()
+	// go MockGCfreq()
+	// go MockMutexCompetition()
+	go MockIOBlock()
 
 	for {
 		time.Sleep(2 * time.Second)
@@ -71,5 +74,71 @@ func MockCPUUse() {
 		for i := 0; i < 10000000; i++ {
 		}
 		time.Sleep(1 * time.Second)
+	}
+}
+
+// 模拟锁竞争
+func MockMutexCompetition() {
+	var mu sync.Mutex
+	data := make(map[int]int)
+
+	runtime.SetMutexProfileFraction(1) // 采样所有的锁竞争事件
+	competition := func(id int) {
+		for {
+			mu.Lock()
+			data[id]++
+			mu.Unlock()
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		go competition(i)
+	}
+}
+
+// 解决锁竞争
+func SolveMutexCompetition() {
+	type dataMutex struct {
+		data []map[int]int
+		mu   []sync.Mutex
+	}
+
+	totalSize := 10
+	dm := dataMutex{
+		data: make([]map[int]int, totalSize),
+		mu:   make([]sync.Mutex, totalSize),
+	}
+	for i := 0; i < totalSize; i++ {
+		dm.data[i] = make(map[int]int)
+	}
+
+	runtime.SetMutexProfileFraction(1)
+
+	for i := 0; i < 100; i++ {
+		go func(id int) {
+			lockID := id % totalSize
+			for j := 0; j < 1000; j++ {
+				dm.mu[lockID].Lock()
+				dm.data[lockID][id]++
+				dm.mu[lockID].Unlock()
+				time.Sleep(10 * time.Millisecond)
+			}
+		}(i)
+	}
+}
+
+// 模拟IO阻塞
+func MockIOBlock() {
+	var mu sync.Mutex
+	runtime.SetBlockProfileRate(1) // 采样所有的阻塞事件
+	for {
+		go func() {
+			mu.Lock()
+			defer mu.Unlock()
+			// 模拟长时间IO阻塞
+			time.Sleep(10 * time.Second)
+		}()
+		time.Sleep(10 * time.Millisecond)
 	}
 }
